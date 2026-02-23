e9eeee01c058f4efb1ac6435582d69b5e8e32dbd
hafeezbalock <63083724+hafeezbalock@users.noreply.github.com>
2026-02-23 20:26:22 +0500

    Update ownerNumber in settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Baloch Bot",
   botOwner: 'Hafeez', // Your name
-  ownerNumber: '923003534702', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '923243252787', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
